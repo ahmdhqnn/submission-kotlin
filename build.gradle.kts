7b63dd3a51fc725345f268e28266b1d43462b5d0
ahmdhqnn <ahmadhaqinn@gmail.com>
2025-01-29 20:13:06 +0700

    done

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,15 @@ repositories {
     google()
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "1.8"
+    targetCompatibility = "1.8"
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
@@ -79,4 +86,5 @@ tasks.register<ExecOperationsTask>("runAllTest") {
         }
     }
 }
+
 
--- src/main/kotlin/com/dicoding/exam/optionalexam4/OptionalExam4.kt
+++ src/main/kotlin/com/dicoding/exam/optionalexam4/OptionalExam4.kt
@@ -2,5 +2,12 @@ package com.dicoding.exam.optionalexam4
 
 // TODO
 fun getMiddleCharacters(string: String): String {
-    return ""
+    val length = string.length
+    val midIndex = length / 2
+
+    return if (length % 2 == 0) {
+        string.substring(midIndex - 1, midIndex + 1)
+    } else {
+        string[midIndex].toString()
+    }
 }
